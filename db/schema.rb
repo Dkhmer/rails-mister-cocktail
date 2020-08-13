de3339c48bc67fd9c6631e1e9c38c0d50443f805
David Kuoch <kuoch0305@gmail.com>
2020-08-13 16:58:58 +0200

    routing,controllers and views for cocktails, doses and ingredients ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_23_091859) do
+ActiveRecord::Schema.define(version: 2020_08_13_100252) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "cocktails", force: :cascade do |t|
     t.string "name"
-    t.integer "dose_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "doses", force: :cascade do |t|
-    t.bigint "ingredient_id", null: false
-    t.bigint "cocktail_id", null: false
     t.string "description"
+    t.bigint "cocktail_id", null: false
+    t.bigint "ingredient_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["cocktail_id"], name: "index_doses_on_cocktail_id"
